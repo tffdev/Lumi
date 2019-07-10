52b72a1be5a27b784d82691370a1acdc66d43ef9
tfcat <danielbri3r@gmail.com>
2019-07-10 09:13:31 +0100

    :white_check_mark: Update tests

--- Core/testing/tests.cpp
+++ Core/testing/tests.cpp
@@ -3,6 +3,7 @@
 #include <filesystem.h>
 #include <objectasset.h>
 #include <luamanager.h>
+#include <configasset.h>
 
 TEST_CASE("Sanity Check") {
     CHECK(1 == 1);
@@ -23,23 +24,47 @@ TEST_CASE("ObjectAsset") {
     CHECK_EQ(asset2.get_default_sprite().compare(""), 0);
 }
 
+TEST_CASE("ConfigAsset") {
+    ConfigAsset asset("Hello", 320, 240, 0x32d61cff);
+    CHECK_EQ(asset.get_window_size().x, 320);
+    CHECK_EQ(asset.get_window_size().y, 240);
+    CHECK_EQ(asset.get_window_title().compare("Hello"), 0);
+    CHECK_EQ(asset.get_window_draw_color().r, 50);
+    CHECK_EQ(asset.get_window_draw_color().g, 214);
+    CHECK_EQ(asset.get_window_draw_color().b, 28);
+    CHECK_EQ(asset.get_window_draw_color().a, 255);
+}
+
 TEST_CASE("FileSystem") {
     SUBCASE("Existence check, basic file reading.") {
         CHECK_EQ(FileSystem::file_exists("hello.txt"), true);
-        CHECK_NE(FileSystem::file_exists("goodbye.txt"), true);
+        CHECK_EQ(FileSystem::file_exists("goodbye.txt"), false);
         CHECK_EQ(FileSystem::read_file("hello.txt").compare("hello\nworld"), 0);
-        CHECK_NE(FileSystem::read_file("goodbye.txt").compare("hello\nworld"), 0);
+
+        // Make sure reading a file that doesn't exist results in a thrown error
+        bool error = false;
+        try { FileSystem::read_file("goodbye.txt"); } catch (...) { error = true; }
+        CHECK_EQ(error, true);
+    }
+    SUBCASE("Utilities") {
+        CHECK_EQ(FileSystem::hex_string_to_uint("0"), 0);
+        CHECK_EQ(FileSystem::hex_string_to_uint("ff0000"), 16711680);
+        CHECK_EQ(FileSystem::hex_string_to_uint("FA842A"), 16417834);
     }
     SUBCASE("Read object file.") {
         CHECK_NE(FileSystem::load_object_file().compare(""), 0);
         CHECK_NE(FileSystem::read_file("objects.xml").compare(""), 0);
         CHECK_EQ(FileSystem::load_object_file().compare(FileSystem::read_file("objects.xml")), 0);
     }
-
     SUBCASE("Load window configuration file into window config object.") {
-
+        ConfigAsset asset = FileSystem::load_config();
+        CHECK_EQ(asset.get_window_size().x, 320);
+        CHECK_EQ(asset.get_window_size().y, 240);
+        CHECK_EQ(asset.get_window_title().compare("Hello World!"), 0);
+        CHECK_EQ(asset.get_window_draw_color().r, 255);
+        CHECK_EQ(asset.get_window_draw_color().g, 0);
+        CHECK_EQ(asset.get_window_draw_color().b, 0);
     }
-
     SUBCASE("Load Objects from XML into vector of ObjectAssets") {
         std::vector<ObjectAsset*> objassets = FileSystem::load_objects();
         CHECK_EQ(objassets.at(0)->get_name().compare("objTest"), 0);
@@ -50,10 +75,20 @@ TEST_CASE("FileSystem") {
 
 TEST_CASE("LuaManager") {
     LuaManager lmanager;
-    CHECK_EQ(lmanager.get_global_int("lua_main_check"), 983652);
-    CHECK_EQ(lmanager.get_global_int("lua_objects_check"), 345897);
-    lmanager.execute("lua_execute_check = 370439");
-    CHECK_EQ(lmanager.get_global_int("lua_execute_check"), 370439);
+    SUBCASE("Lua execution check") {
+        lmanager.execute("lua_execute_check = 370439");
+        CHECK_EQ(lmanager.get_global_int("lua_execute_check"), 370439);
+        CHECK_EQ(lmanager.get_global_int("nonexistant_variable_check"), 0);
+    }
+    SUBCASE("Load main and objects check"){
+        CHECK_EQ(lmanager.get_global_int("lua_main_check"), 983652);
+        CHECK_EQ(lmanager.get_global_int("lua_objects_check"), 345897);
+    }
+    SUBCASE("Invalid code throw check"){
+        bool error = false;
+        try { lmanager.execute("lk hsd dglg"); } catch (...) { error = true; }
+        CHECK_EQ(error, true);
+    }
 }
 
 
