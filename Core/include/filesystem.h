ec9cee8a91f36a6184201c552bab69b317909aa9
tfcat <danielbri3r@gmail.com>
2019-07-10 09:18:37 +0100

    :sparkles: Filesystem loads config file

--- Core/include/filesystem.h
+++ Core/include/filesystem.h
@@ -4,10 +4,13 @@
 #include <pugixml.hpp>
 #include <vector>
 #include <objectasset.h>
+#include <configasset.h>
+#include <sstream>
 
 #define DATA_PATH "../data/"
 
 #define OBJECT_PATH "objects.xml"
+#define CONFIG_PATH "config.xml"
 
 /**
  * FileSystem currently reads directly from the OS's regular file system.
@@ -18,9 +21,12 @@
 namespace FileSystem {
     std::string load_object_file();
     std::string load_sprite_file();
+    std::string load_config_file();
+    unsigned int hex_string_to_uint(std::string str);
     bool file_exists(std::string filename);
     std::string read_file(std::string filename);
 
+    ConfigAsset load_config();
     std::vector<ObjectAsset*> load_objects();
     std::vector<int> load_sprites();
     std::vector<int> load_backgrounds();
--- Core/source/filesystem.cpp
+++ Core/source/filesystem.cpp
@@ -10,7 +10,7 @@ bool FileSystem::file_exists(std::string filename) {
 }
 
 std::string FileSystem::read_file(std::string filename) {
-    if(!file_exists(DATA_PATH + filename)) return std::string();
+    if(!file_exists(DATA_PATH + filename)) throw "File " + filename + " doesn't exist!";
     std::ifstream stream(DATA_PATH + filename);
     std::string str((std::istreambuf_iterator<char>(stream)),
                      std::istreambuf_iterator<char>());
@@ -18,10 +18,13 @@ std::string FileSystem::read_file(std::string filename) {
 }
 
 std::string FileSystem::load_object_file() {
-    if(!file_exists(OBJECT_PATH))
-        throw "Cannot load Object XML file.";
-    std::string file_data = read_file(OBJECT_PATH);
-    return file_data;
+    if(!file_exists(OBJECT_PATH)) throw "Cannot load Object XML file.";
+    return read_file(OBJECT_PATH);;
+}
+
+std::string FileSystem::load_config_file() {
+    if(!file_exists(CONFIG_PATH)) throw "Cannot load Config XML file.";
+    return read_file(CONFIG_PATH);
 }
 
 std::vector<ObjectAsset*> FileSystem::load_objects() {
@@ -34,6 +37,27 @@ std::vector<ObjectAsset*> FileSystem::load_objects() {
                                     obj_xml.child("name").text().as_string(),
                                     obj_xml.child("sprite").text().as_string()));
     }
-
     return object_vector;
+}
+
+unsigned int FileSystem::hex_string_to_uint(std::string str) {
+    unsigned int x;
+    std::stringstream ss;
+    ss << std::hex << str;
+    ss >> x;
+    return static_cast<unsigned int>(x);
+}
+
+ConfigAsset FileSystem::load_config() {
+    pugi::xml_document document;
+    document.load_string(FileSystem::load_config_file().c_str());
+
+    ConfigAsset config_asset(
+                document.child("window").child("windowtitle").text().as_string(),
+                document.child("window").child("windowsize").child("width").text().as_int(),
+                document.child("window").child("windowsize").child("height").text().as_int(),
+                hex_string_to_uint(std::string(document.child("window").child("windowdrawcolor").text().as_string()) + "ff")
+                );
+
+    return config_asset;
 }
